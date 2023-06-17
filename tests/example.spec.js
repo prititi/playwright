d918139e4567744d9dfa3d79eb91457f75e36bf4
prititi <pt446914@gmail.com>
2023-06-17 18:22:39 +0530

    google platwright

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -53,3 +53,11 @@ test("get started link", async ({ page }) => {
 
   // await expect(page).toHaveURL(/.*confirmation/);
 });
+
+test("my first test", async({browser})=>{
+  const context= await browser.newContext();
+  const page= await context.newPage();
+  await page.goto("https://www.google.com");
+  console.log(await page.title());
+  await expect(page).toHaveTitle("Google")
+});
---
+++ tests/googleTest.spec.js
@@ -0,0 +1,9 @@
+// const {test, expect}= require("@playwright/test")
+
+// test("my first test", async({browser})=>{
+//     const context= await browser.newContext();
+//     const page= await context.newPage();
+//     await page.goto("https://www.google.com");
+//     console.log(await page.title());
+//     await expect(page).toHaveTitle("Google")
+// });
